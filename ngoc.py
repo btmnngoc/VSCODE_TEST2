78c9c757c9b9f2d1c4f1ead155db99758706c30b
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-17 04:37:48 +0700

    test

--- ngoc.py
+++ ngoc.py
@@ -1,20 +1,6 @@
 import streamlit as st
 
-st.title("NGOC - test")
-st.write("This is a test for the NGOC app.")
-st.write("this is a new test")
+st.write("NGOC - Next Generation Open Chatbot")
 
-a = st.button("Click me!")
-if a:
-    st.write("you clicked the button!")
-st.text_input("Enter some text:")
-st.text_area("Enter some text:")
-st.selectbox("Select an option:", ["Option 1", "Option 2", "Option 3"])
-st.multiselect("Select multiple options:", ["Option 1", "Option 2", "Option 3"])
-st.slider("Select a range:", 0, 100, (25, 75))
-st.checkbox("Check this box")
-st.radio("Select one option:", ["Option 1", "Option 2", "Option 3"])
+st.write("This is a simple chatbot application that uses the OpenAI API to generate responses to user input.")
 
-st.write("This is a new test for the NGOC app.")
-
-st.write("This is a new test for the NGOC app.")
