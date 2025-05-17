e6c51e0d21f69c9975cd787df4f369e82b0ec053
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-17 18:02:45 +0700

    update

--- ngoc.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("NGOC - Next Generation Open Chatbot")
-
-st.write("This is a simple chatbot application that uses the OpenAI API to generate responses to user input.")
-
